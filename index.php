d62a94f0f5d451972a00d578e2aae761b0e22ee4
padalack <kallriq@gmail.com>
2018-07-18 11:01:54 -0400

    1.0.0.4

--- index.php
+++ index.php
@@ -4,14 +4,19 @@ require_once ("vendor/autoload.php");
 //require_once ("vendor/padalack/php-classes/src/Page.php");
 use \Slim\Slim;
 use \Padalack\Page;
+use \Padalack\PageAdmin;
 $app= new Slim();
 $app->config('debug',true);
+
 $app->get('/', function (){
     $page = new Page();
     $page->setTpl("index");
-    /*$sql = new \Padalack\DB\Sql();
-    $results = $sql->select("SELECT * FROM tb_users");
-    echo json_encode($results);*/
 });
+
+$app->get('/admin', function (){
+    $page = new PageAdmin();
+    $page->setTpl("index");
+});
+
 $app->run();
 
